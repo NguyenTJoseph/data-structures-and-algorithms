58d8501a153f453d076a6f2430281fe78dda06d4
Joseph Nguyen <josephnguyen2@gmail.com>
2021-12-05 16:26:14 -0800

    challenge 5

---
+++ python/code_challenges/Challenge 5/linked-list/linked_list/list.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, value):
+        node = Node(value)
+        node.next = self.head
+        self.head = node
+
+    def includes(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
+    def toString(self):
+        current = self.head
+        string = ""
+        while current:
+            string += "{ " + current.value + " } -> "
+            current = current.next
+
+        string += "NULL"
+        return string
