0aacd942fbe5a51351d92a64bec2b829fee8ed87
Joseph Nguyen <josephnguyen2@gmail.com>
2021-12-12 18:58:40 -0800

    stacks&queues

---
+++ python/code_challenges/Challenge 9/stack_and_queue.py
@@ -0,0 +1,73 @@
+class Node:
+    def __init__(self, value, next = None):
+        self.value = value
+        self.next = next
+
+class InvalidOperationError(Exception):
+    pass
+
+class Stack:
+    def __init__(self, top=None):
+        self.top = top
+
+    def push(self, value):
+        top = self.top
+        if top:
+            self.top = Node(value, top)
+        self.top = Node(value, top)
+
+    def pop(self):
+
+        if not self.top:
+            raise InvalidOperationError(
+                "Not allowed on empty collection"
+            )
+        pop = self.top
+        self.top = self.top.next
+        return pop.value
+
+    def peek(self):
+
+        if not self.top:
+            raise InvalidOperationError(
+                "Not allowed on empty collection"
+            )
+
+        return self.top.value
+
+    def is_empty(self):
+        return self.top is None
+
+
+class Queue:
+    def __init__(self, front=None):
+        self.front = None
+        self.rear = None
+
+    def enqueue(self, value):
+        node = Node(value)
+        if self.rear:
+            self.rear.next = node
+        else:
+            self.front = node
+        self.rear = node
+
+    def dequeue(self):
+        if not self.front:
+            raise InvalidOperationError(
+                "Not allowed on empty collection"
+            )
+        node = self.front
+        self.front = self.front.next
+        node.next = None
+        return node.value
+
+    def peek(self):
+        if not self.front:
+            raise InvalidOperationError(
+                "Not allowed on empty collection"
+            )
+        return self.front.value
+
+    def is_empty(self):
+        return self.front is None
