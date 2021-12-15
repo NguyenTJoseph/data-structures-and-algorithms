bf72cfa1fcaff6bbd96986a16c166852e2cfce21
3lueHippo <josephnguyen2@gmail.com>
2021-12-15 09:57:18 -0800

    Update stack_queue_bracket.py

---
+++ python/Challenge 13/stack_queue_bracket.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+def validate_brackets(string):
+    base = {'(': 0, ')': 0, '[': 0, ']': 0, '{': 0, '}': 0}
+    counts = dict(Counter(string))
+    counts = {**base, **counts}
+    return counts['('] == counts[')'] and counts['['] == counts[']'] and counts['{'] == counts['}'] 
