ac39a141f0e4f4f97ad29ff8f3bdc7d566cf8b2b
Joseph Nguyen <42249177+3lueHippo@users.noreply.github.com>
2022-01-13 22:08:24 -0800

    Testing

--- python/code_challenges/algorithm/tree_intersection/tree_intersection.py
+++ python/code_challenges/algorithm/tree_intersection/tree_intersection.py
@@ -6,6 +6,7 @@ class Node:
 
 
 from hashtable import HashTable 
+
 def tree_intersection(head, morehead):
     ht = HashTable()
     q = [head, morehead]
@@ -35,30 +36,30 @@ def tree_intersection(head, morehead):
     return repeats
 
 
-def test_inter1():
-    a = Node(12, Node(20, Node(15)))  
-    b = Node(12, Node(20, Node(15))) 
-    actual = tree_intersection(a, b)
-    expected = [12, 20, 15]
-    assert actual == expected
-    print("test passed")
+# def test_inter1():
+#     a = Node(12, Node(20, Node(15)))  
+#     b = Node(12, Node(20, Node(15))) 
+#     actual = tree_intersection(a, b)
+#     expected = [12, 20, 15]
+#     assert actual == expected
+#     print("test passed")
 
-def test_inter2():
-    a = Node(2, Node(5, Node(12)), Node(1, Node(15)))  
-    b = Node(4, Node(2, Node(3))) 
-    actual = tree_intersection(a, b)
-    expected = [2]
-    assert actual == expected
-    print("test passed")
+# def test_inter2():
+#     a = Node(2, Node(5, Node(12)), Node(1, Node(15)))  
+#     b = Node(4, Node(2, Node(3))) 
+#     actual = tree_intersection(a, b)
+#     expected = [2]
+#     assert actual == expected
+#     print("test passed")
 
-def test_inter3():
-    a = Node(2, Node(5, Node(12)), Node(1, Node(15)))  
-    b = Node(4, Node(21, Node(7)), Node(10, Node(27))) 
-    actual = tree_intersection(a, b)
-    expected = []
-    assert actual == expected
-    print("test passed")
+# def test_inter3():
+#     a = Node(2, Node(5, Node(12)), Node(1, Node(15)))  
+#     b = Node(4, Node(21, Node(7)), Node(10, Node(27))) 
+#     actual = tree_intersection(a, b)
+#     expected = []
+#     assert actual == expected
+#     print("test passed")
 
-test_inter1()
-test_inter2()
-test_inter3()
+# test_inter1()
+# test_inter2()
+# test_inter3()
